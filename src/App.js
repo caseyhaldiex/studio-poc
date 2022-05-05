eac3d2c142468378b32f7cc64c8d078d05c22ba0
Casey Hald <casey.hald@Caseys-MacBook-Pro-2.local>
2022-05-05 11:28:51 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import * as htmlToImage from 'html-to-image';
+import { toPng, download } from 'html-to-image';
+import Draggable, {DraggableCore} from 'react-draggable'; 
+import { render } from "react-dom";
+import { Resizable } from "re-resizable";
+import { useState } from 'react';
+
+const makeImage = () => {
+  htmlToImage.toPng(document.getElementById('my-node'))
+  .then(function (dataUrl) {
+    console.log(dataUrl, 'my-node.png')
+  });
+}
+
+const style = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "solid 1px #ddd",
+};
 
 function App() {
+  const [title, setTitle] = useState("");
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+
+    <div class="img-size" id="my-node">
+      <p>{title}</p>
+      
+      <h1>OMG</h1>
+      
+
+      <Draggable>
+        <Resizable
+          style={style}
+          defaultSize={{
+            width: 200,
+            height: 200
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className={"size-class"}></div>
+        </Resizable>
+      </Draggable>
+        
     </div>
+    <input onChange={e => setTitle(e.target.value)} value={title}></input>
+    <button onClick={makeImage} className="App">
+      Make an image
+    </button>
+    </>
+    
   );
 }
 
